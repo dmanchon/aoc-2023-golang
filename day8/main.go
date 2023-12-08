7ffdfc5ceca4d5a0ef7c81fd8aa9893856584e8e
Daniel Manchon <dmanchon@gmail.com>
2023-12-08 14:27:45 +0100

    day8

--- day8/main.go
+++ day8/main.go
@@ -6,7 +6,30 @@ import (
 	"strings"
 )
 
-func solve1(input string) int {
+func gcd(a, b int) int {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
+}
+
+// Function to calculate LCM (Least Common Multiple)
+func lcm(a, b int) int {
+	return a * b / gcd(a, b)
+}
+
+// Function to calculate LCM of a list of integers
+func LCM(numbers []int) int {
+	result := numbers[0]
+	for i := 1; i < len(numbers); i++ {
+		result = lcm(result, numbers[i])
+	}
+	return result
+}
+
+func ParseInput(input string) ([]byte, map[string][]string) {
 	split := strings.Split(input, "\n\n")
 	instructions := []byte(strings.Split(split[0], "\n")[0])
 	nodes := make(map[string][]string, 0)
@@ -23,6 +46,11 @@ func solve1(input string) int {
 
 		nodes[node] = []string{left, right}
 	}
+	return instructions, nodes
+}
+
+func solve1(input string) int {
+	instructions, nodes := ParseInput(input)
 
 	pos := "AAA"
 	i := 0
@@ -45,30 +73,14 @@ func solve1(input string) int {
 }
 
 func solve2(input string) int {
-	split := strings.Split(input, "\n\n")
-	instructions := []byte(strings.Split(split[0], "\n")[0])
-	nodes := make(map[string][]string, 0)
-	positions := make([]string, 0)
-	for _, node_str := range strings.Split(split[1], "\n") {
-		var node, left, right string
-		split1 := strings.Split(node_str, " = ")
-
-		node = split1[0]
-
-		split2 := strings.Split(split1[1], ", ")
-		left = split2[0][1:]
-		right = split2[1][:len(split2[1])-1]
-
-		nodes[node] = []string{left, right}
-		if []byte(node)[2] == 'A' {
-			positions = append(positions, node)
-		}
-	}
+	instructions, nodes := ParseInput(input)
 
 	loops := make([]int, 0)
-	for _, pos := range positions {
+	for pos := range nodes {
+		if []byte(pos)[2] != 'A' {
+			continue
+		}
 		i := 0
-
 		for {
 			ip := i % len(instructions)
 			if instructions[ip] == 'L' {
@@ -87,29 +99,6 @@ func solve2(input string) int {
 		}
 	}
 	return LCM(loops)
-}
-
-func gcd(a, b int) int {
-	for b != 0 {
-		t := b
-		b = a % b
-		a = t
-	}
-	return a
-}
-
-// Function to calculate LCM (Least Common Multiple)
-func lcm(a, b int) int {
-	return a * b / gcd(a, b)
-}
-
-// Function to calculate LCM of a list of integers
-func LCM(numbers []int) int {
-	result := numbers[0]
-	for i := 1; i < len(numbers); i++ {
-		result = lcm(result, numbers[i])
-	}
-	return result
 }
 
 func main() {
